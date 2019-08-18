cfbb676753b75e2fa0c34329f347d3d007f6f062
Gifary Dhimas <gifarydhimas@gmail.com>
2019-08-18 09:08:53 +0700

    Integrate new GQL schema to connect/disconnect storage provider
    Changes:
     - Integrate new GraphQL schema to connect / disconnect storage provider
     - Revamp Authorize / Unauthorize storage provider UI

--- src/config.js
+++ src/config.js
@@ -17,15 +17,17 @@ export const storageProviders = [
   {
     id: 12345678,
     name: "Dropbox",
+    imageUrl: "/img/dropbox-logo-sm.png",
     redirectUri: 'http://localhost:3000/account/storage/authorize?provider=dropbox',
     clientId: '0ljk1sqdvxnhd3i',
-    authorizationUrl: `https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=0ljk1sqdvxnhd3i&redirect_uri=http://localhost:3000/account/storage/authorize?provider=dropbox`
+    authorizationUrl: `https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=0ljk1sqdvxnhd3i&redirect_uri=http://localhost:3000/account/storage/authorize?providerId=12345678`
   },
-  // {
-  //   id: 88181612,
-  //   name: "Google Drive",
-  //   redirectUri: 'http://localhost:3000/account/storage/authorize?provider=dropbox',
-  //   clientId: '0ljk1sqdvxnhd3i',
-  //   authorizationUrl: `https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=0ljk1sqdvxnhd3i&redirect_uri=http://localhost:3000/account/storage/authorize?provider=dropbox`
-  // },
+  {
+    id: 88181612,
+    name: "Google Drive",
+    imageUrl: "/img/google-drive-logo-sm.png",
+    redirectUri: 'http://localhost:3000/account/storage/authorize?provider=dropbox',
+    clientId: '0ljk1sqdvxnhd3i',
+    authorizationUrl: `https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=0ljk1sqdvxnhd3i&redirect_uri=http://localhost:3000/account/storage/authorize?providerId=88181612`
+  },
 ]
