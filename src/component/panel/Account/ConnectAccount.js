b94f2ef5c5e6ae78ed5980f4e575d666c287481f
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-07-21 23:05:36 +0700

    add continue button

--- src/component/panel/Account/ConnectAccount.js
+++ src/component/panel/Account/ConnectAccount.js
@@ -1,13 +1,15 @@
 import { Button } from "@bccfilkom/designsystem/build";
-import React from "react";
+import React, { useState } from "react";
 import style from "../../../css/account-connect.module.scss";
 
 const ConnectAccount = () => {
+  const [useDrive, setUseDrive] = useState(false);
+
   return (
     <div className={style["container"]}>
       <div className={style["title-container"]}>
         <h1>Tautkan Akun Anda Sekarang!</h1>
-        <p>Integrasikan akun anda dengan cloud storage Dropbox </p>
+        <p>Integrasikan akun anda dengan cloud storage </p>
       </div>
       <div className={style["card-container"]}>
         <div className={style["card"] + " " + style["card-active"]}>
@@ -19,37 +21,35 @@ const ConnectAccount = () => {
             <p className={style["card__body__description"]}>
               Tautkan akun ke Dropbox untuk menyimpan file
             </p>
-            <Button>Tautkan Akun</Button>
+            {useDrive ? (
+              <Button
+                className={style["button-cancel"]}
+                onClick={() => setUseDrive(false)}
+              >
+                Batalkan{" "}
+              </Button>
+            ) : (
+              <Button onClick={() => setUseDrive(true)}>Tautkan </Button>
+            )}
           </div>
         </div>
         <div className={style["card"]}>
           <div className={style["card__img"]}>
-            <img src="/img/icons/drive-inactive.svg" alt="drive" />
+            <img src="/img/icons/drive-active.svg" alt="drive" />
           </div>
           <div className={style["card__body"]}>
-            <h3
-              className={
-                style["card__body__title"] +
-                " " +
-                style["card__body__title-disabled"]
-              }
-            >
-              Google Drive
-            </h3>
-            <p
-              className={
-                style["card__body__description"] +
-                " " +
-                style["card__body__description-disabled"]
-              }
-            >
-              Tautkan akun ke Google Drive untuk menyimpan file
+            <div className={style["card__body__badge"]}>
+              <span>Coming Soon</span>
+            </div>
+            <h3 className={style["card__body__title"]}>Google Drive</h3>
+            <p className={style["card__body__description"]}>
+              Nantikan fitur baru untuk dapat terhubung ke Google Drive
             </p>
-            <Button type="secondary" disabled>
-              Tautkan Akun
-            </Button>
           </div>
         </div>
+      </div>
+      <div className={style["button-container"]}>
+        <Button type="secondary">Lanjutkan</Button>
       </div>
     </div>
   );
