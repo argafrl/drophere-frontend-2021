1163e7bfd11de97b9d10cf0f4719df4a8f0a022a
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-08-23 10:16:38 +0700

    auto update service worker

--- src/index.js
+++ src/index.js
@@ -15,4 +15,14 @@ ReactDOM.render(<App />, document.getElementById("app"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: (e) => {
+    const { waiting: { postMessage = null } = {}, update } = e || {};
+    if (postMessage) {
+      postMessage({ type: "SKIP_WAITING" });
+    }
+    update().then(() => {
+      window.location.reload();
+    });
+  },
+});
